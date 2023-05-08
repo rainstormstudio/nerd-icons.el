6fdb423eabb1c14161cb59af8fd74ee8b572e303
Daniel <rainstormstudio@yahoo.com>
2023-05-08 12:23:34 -0400

    add nerd-icons-data.el
    put all of nerd-icons-data related `require` to one file.
    
    This is related to #19

---
+++ nerd-icons-data.el
@@ -0,0 +1,44 @@
+;;; nerd-icons-data.el --- Nerd Icons data -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Hongyu Ding <rainstormstudio@yahoo.com>
+
+;; Author: Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang <seagle0128@gmail.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/rainstormstudio/nerd-icons.el
+;; Keywords: convenient, lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; nerd icons data
+
+;;; Code:
+
+(require 'nerd-icons-data-iec-power-symbols "./data/nerd-icons-data-iec-power-symbols")
+(require 'nerd-icons-data-pomicons "./data/nerd-icons-data-pomicons")
+(require 'nerd-icons-data-octicons "./data/nerd-icons-data-octicons")
+(require 'nerd-icons-data-powerline "./data/nerd-icons-data-powerline")
+(require 'nerd-icons-data-font-awesome "./data/nerd-icons-data-font-awesome")
+(require 'nerd-icons-data-weather-icons "./data/nerd-icons-data-weather-icons")
+(require 'nerd-icons-data-seti-ui-custom "./data/nerd-icons-data-seti-ui-custom")
+(require 'nerd-icons-data-devicons "./data/nerd-icons-data-devicons")
+(require 'nerd-icons-data-codicons "./data/nerd-icons-data-codicons")
+(require 'nerd-icons-data-font-logos "./data/nerd-icons-data-font-logos")
+(require 'nerd-icons-data-material-design "./data/nerd-icons-data-material-design")
+
+(provide 'nerd-icons-data)
+;;; nerd-icons-data.el ends here
--- nerd-icons.el
+++ nerd-icons.el
@@ -38,17 +38,7 @@
 
 (require 'cl-lib)
 
-(require 'nerd-icons-data-iec-power-symbols "./data/nerd-icons-data-iec-power-symbols")
-(require 'nerd-icons-data-pomicons "./data/nerd-icons-data-pomicons")
-(require 'nerd-icons-data-octicons "./data/nerd-icons-data-octicons")
-(require 'nerd-icons-data-powerline "./data/nerd-icons-data-powerline")
-(require 'nerd-icons-data-font-awesome "./data/nerd-icons-data-font-awesome")
-(require 'nerd-icons-data-weather-icons "./data/nerd-icons-data-weather-icons")
-(require 'nerd-icons-data-seti-ui-custom "./data/nerd-icons-data-seti-ui-custom")
-(require 'nerd-icons-data-devicons "./data/nerd-icons-data-devicons")
-(require 'nerd-icons-data-codicons "./data/nerd-icons-data-codicons")
-(require 'nerd-icons-data-font-logos "./data/nerd-icons-data-font-logos")
-(require 'nerd-icons-data-material-design "./data/nerd-icons-data-material-design")
+(require 'nerd-icons-data)
 
 (require 'nerd-icons-faces)
 
